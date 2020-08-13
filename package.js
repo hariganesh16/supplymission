9dbd7f03b9c4073cb542a08fddcb690e43ec626b
hariganesh16 <68473033+hariganesh16@users.noreply.github.com>
2020-08-13 20:56:46 +0530

    Add files via upload

---
+++ package.js
@@ -0,0 +1,10 @@
+class Package {
+    constructor(x,y,width,height){
+        this.image=loadImage("sprites/package.png");
+        var options = {
+        isStatic:true
+    }
+    }
+    display(){
+         }
+}        
